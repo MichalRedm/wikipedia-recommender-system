558a493d742844eabb311f23fafd14f44683572d
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-12-27 11:15:42 +0100

    Correct way of saving the recommender, progress bar for stemming

--- wikirecommender/processing.py
+++ wikirecommender/processing.py
@@ -4,12 +4,19 @@ from nltk.tokenize import word_tokenize
 from typing import List
 
 def stemmer(string: str) -> List[str]:
-    """Stem the words and remove stopwords."""
+    """
+    Stem the words and remove stopwords.
+
+    Args:
+        string (str): The input string to process.
+
+    Returns:
+        List[str]: A list of stemmed words with stopwords removed.
+    """
     porter = PorterStemmer()
-    stemmed_words = (
-        porter.stem(x)
-        for x in word_tokenize(string.replace('\n',' '))
-    )
+    tokens = word_tokenize(string.replace('\n', ' '))
+    
+    stemmed_words = (porter.stem(x) for x in tokens)
     return [
         x for x in stemmed_words
         if x not in stopwords.words('english') and x.isalpha()
--- wikirecommender/recommender.py
+++ wikirecommender/recommender.py
@@ -2,6 +2,7 @@ import pickle
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from tqdm.auto import tqdm
 
 from wikirecommender.scraping import wikipedia_scrapper, wikipedia_scrapper_single_page
 from wikirecommender.processing import stemmer
@@ -14,12 +15,18 @@ class WikipediaRecommender:
         self.dataset = None
         self.vectorizer = None
 
-    def load_articles(self, page_count: int = 20, start_link="https://en.wikipedia.org/wiki/Wikipedia:Popular_pages"):
+    def load_articles(self, page_count: int = 20, start_link="https://en.wikipedia.org/wiki/Wikipedia:Popular_pages", verbose: bool = True) -> None:
         """Fetch and process articles to create a dataset with TF-IDF representation."""
-        df = wikipedia_scrapper(start_link, page_count)
-        df['stemmed_words'] = df['text'].apply(stemmer)
+        df = wikipedia_scrapper(start_link, page_count, verbose=verbose)
+        if verbose:
+            tqdm.pandas(desc="Processing articles")
+            df['stemmed_words'] = df['text'].progress_apply(stemmer)
+        else:
+            df['stemmed_words'] = df['text'].apply(stemmer)
         df['processed_text'] = df['stemmed_words'].apply(lambda words: " ".join(words))
         
+        if verbose:
+            print("Creating TF-IDF matrix...")
         self.vectorizer = TfidfVectorizer()
         tfidf_matrix = self.vectorizer.fit_transform(df['processed_text'])
         tfidf_df = pd.DataFrame(tfidf_matrix.toarray(), columns=self.vectorizer.get_feature_names_out())
@@ -58,12 +65,46 @@ class WikipediaRecommender:
         return result_df
     
     def save_to_file(self, filename: str) -> None:
-        """Save the current instance to a file using pickle."""
-        with open(filename, 'wb') as file:
-            pickle.dump(self, file)
+        """Save the current instance to a CSV file."""
+        # Ensure the filename has the correct extension
+        if not filename.lower().endswith('.csv'):
+            filename += '.csv'
+
+        vocabulary = self.vectorizer.vocabulary_
+        idf_values = self.vectorizer.idf_
+
+        # Convert to DataFrame
+        df_vocab = pd.DataFrame(list(vocabulary.items()), columns=['term', 'index'])
+        df_idf = pd.DataFrame(idf_values, index=list(vocabulary.keys()), columns=['idf'])
+
+        df = df_vocab.join(df_idf, on='term')
+        df.sort_values(by='index', inplace=True)
+        df.drop(columns=['index'], inplace=True)
+        df.set_index('term', inplace=True)
+        df = df.transpose()
+        df = pd.concat([df, self.dataset], axis=0)
+
+        df.set_index('wikipedia_url', inplace=True)
+            
+        df.to_csv(filename)
 
     @staticmethod
     def load_from_file(filename: str) -> 'WikipediaRecommender':
-        """Load an instance from a file using pickle."""
-        with open(filename, 'rb') as file:
-            return pickle.load(file)
+        """Load an instance from a CSV file."""
+        # Ensure the filename has the correct extension
+        if not filename.lower().endswith('.csv'):
+            filename += '.csv'
+    
+        df = pd.read_csv(filename, index_col='wikipedia_url')
+
+        vocabulary = dict(zip(df.columns, range(len(df.columns))))
+        idf_values = df.iloc[0].values
+        dataset = df.iloc[1:]
+
+        recommender = WikipediaRecommender()
+        recommender.vectorizer = TfidfVectorizer(vocabulary=vocabulary)
+        recommender.vectorizer.idf_ = idf_values
+        recommender.dataset = dataset.reset_index()
+
+        return recommender
+
