a398e43edc25ed53246d0bcdc6bd41063b431fc7
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-12-28 09:49:49 +0100

    Handle redirections in scraping

--- wikirecommender/scraping.py
+++ wikirecommender/scraping.py
@@ -12,11 +12,11 @@ def wikipedia_scrapper_single_page(url: str) -> str:
     if response.status_code != 200:
         raise Exception(f"Failed to fetch the URL: {url} with status code {response.status_code}")
     
-    # Extract the text from the <div id="bodyContent">
+    # Extract the text from the <div id="mw-content-text">
     parsed = BeautifulSoup(response.text, 'html.parser')
-    body_content = parsed.find('div', id='bodyContent')
+    body_content = parsed.find('div', id='mw-content-text')
     if body_content is None:
-        raise Exception(f"Could not find <div id='bodyContent'> in the page: {url}")
+        raise Exception(f"Could not find <div id='mw-content-text'> in the page: {url}")
     new_text = body_content.get_text(strip=True)
 
     return new_text
@@ -34,14 +34,21 @@ def wikipedia_scrapper(link: str, page_count: int = 20, *, verbose: bool = True,
             current_link = queue.pop()
             if current_link in visited:
                 continue
-            visited.add(current_link)
 
             response = requests.get(current_link)
             if response.status_code != 200:
                 raise Exception(f"Website returned status code {response.status_code}.")
             
             parsed = BeautifulSoup(response.text, 'html.parser')
-            body_content = parsed.find('div', id='bodyContent')
+
+            # Handle redirects by resolving the final URL
+            resolved_link = parsed.find('link', rel='canonical')["href"]
+            if resolved_link in visited:
+                continue
+
+            visited.add(resolved_link)
+
+            body_content = parsed.find('div', id='mw-content-text')
 
             if body_content is None:
                 continue
@@ -49,8 +56,8 @@ def wikipedia_scrapper(link: str, page_count: int = 20, *, verbose: bool = True,
             links = body_content.find_all('a', attrs={'href': re.compile(r'^/wiki')})
             queue[:0] = ["https://en.wikipedia.org" + link['href'] for link in links if ":" not in link['href']]
 
-            if "List_of" not in current_link:
-                result.append({"wikipedia_url": current_link, "text": body_content.get_text(strip=True)})
+            if "List_of" not in resolved_link:
+                result.append({"wikipedia_url": resolved_link, "text": body_content.get_text(strip=True)})
                 pbar.update(1)
 
             sleep((max_sleep_time - min_sleep_time) * random.random() + min_sleep_time)
